2c2ffb1898088199431fb990f5b2fbbc1492979d
Saimum Arafat <101015625+saimumarafat@users.noreply.github.com>
2026-04-25 18:39:38 +0600

    Initial Commit

---
+++ Arrive/ArriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArriveApp.swift
+//  Arrive
+//
+//  Created by Saimum Arafat on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ArriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
